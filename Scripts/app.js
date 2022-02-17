e0a96294fd893fea1deb23f194664c94a07c8d05
fatemameem <48911993+fatemameem@users.noreply.github.com>
2022-02-17 19:56:39 +0600

    JS added

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,17 +12,18 @@ function validate(id) {
     return true;
   }
 }
+let balances = 0;
+// let incomeAmount = 0;
+let income = 0;
 function calculateExpenses() {
-  // console.log("button clicked");
-  const incomeAmount = document.getElementById("income-amount");
   const food = getInputValue("food");
   const rent = getInputValue("rent");
   const clothes = getInputValue("clothes");
+  const incomeAmount = document.getElementById("income-amount");
   const totalExpenses = document.getElementById("expenses-total");
   const totalBalance = document.getElementById("balance");
-  const income = parseFloat(incomeAmount.value);
+  income = parseFloat(incomeAmount.value);
   let expenses = 0;
-  console.log(income);
   if (
     !validate(income) ||
     !validate(food) ||
@@ -33,6 +34,7 @@ function calculateExpenses() {
   } else {
     console.log(food, rent, clothes);
     expenses = food + rent + clothes;
+    balances = income - expenses;
   }
   if (expenses > income) {
     alert("Expenses cannot be greater than income");
@@ -40,10 +42,35 @@ function calculateExpenses() {
   } else {
     console.log(totalExpenses.innerText);
     totalExpenses.innerText = expenses.toFixed(2);
-    totalBalance.innerText = (income - expenses).toFixed(2);
+    totalBalance.innerText = balances.toFixed(2);
     incomeAmount.value = "";
+  }
+}
+function calculateSavings() {
+  const savingsPercentage = document.getElementById("saving-percentage");
+  const incomeBalanceValue = document.getElementById("income-amount");
+  const savings = document.getElementById("saving-balance");
+  const remainingBalance = document.getElementById("remaining-balance");
+  const savingsAmount = parseFloat(savingsPercentage.value);
+  if (savingsAmount < 0 || isNaN(savingsAmount)) {
+    alert("Please enter a valid number");
+    return;
+  } else {
+    savingCalculation = income * (savingsAmount / 100);
+    if (savingCalculation >= balances) {
+      alert("Savings cannot be greater than balance");
+      savingsPercentage.value = "";
+      return;
+    } else {
+      savings.innerText = savingCalculation.toFixed(2);
+      remainingBalance.innerText = (balances - savingCalculation).toFixed(2);
+      savingsPercentage.value = "";
+    }
   }
 }
 document
   .getElementById("calculate-expenses")
   .addEventListener("click", calculateExpenses);
+document
+  .getElementById("calculate-savings")
+  .addEventListener("click", calculateSavings);
