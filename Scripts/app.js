c47cb0d31e432c080fdf48d695ac3af2511c360c
fatemameem <48911993+fatemameem@users.noreply.github.com>
2022-02-17 20:45:16 +0600

    Alert messages added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,20 +1,22 @@
+// Getting Input value from the user for expenses
 function getInputValue(id) {
   let inputVal = document.getElementById(id + "-expense");
   const value = parseFloat(inputVal.value);
   inputVal.value = "";
   return value;
 }
+// Validating the input value from the user
 function validate(id) {
   if (id < 0 || isNaN(id)) {
-    alert("Please enter a valid number");
     return false;
   } else {
     return true;
   }
 }
+// initiating variables for calculation of the expenses and savings
 let balances = 0;
-// let incomeAmount = 0;
 let income = 0;
+// calculation of expenses and showing an alert if the number is invalid
 function calculateExpenses() {
   const food = getInputValue("food");
   const rent = getInputValue("rent");
@@ -22,14 +24,30 @@ function calculateExpenses() {
   const incomeAmount = document.getElementById("income-amount");
   const totalExpenses = document.getElementById("expenses-total");
   const totalBalance = document.getElementById("balance");
+  const alertMessage = document.getElementById("alertBox");
+  const calculationAlert = document.getElementById("alertMsg");
+  const alerts = document.getElementById("alertInput");
   income = parseFloat(incomeAmount.value);
   let expenses = 0;
-  if (
-    !validate(income) ||
-    !validate(food) ||
-    !validate(rent) ||
-    !validate(clothes)
-  ) {
+  if (!validate(income)) {
+    alerts.innerText = "income";
+    alertMessage.style.display = "block";
+    incomeAmount.value = "";
+    return;
+  } else if (!validate(food)) {
+    alerts.innerText = "food";
+    alertMessage.style.display = "block";
+    incomeAmount.value = "";
+    return;
+  } else if (!validate(rent)) {
+    alerts.innerText = "rent";
+    alertMessage.style.display = "block";
+    incomeAmount.value = "";
+    return;
+  } else if (!validate(clothes)) {
+    alerts.innerText = "clothes";
+    alertMessage.style.display = "block";
+    incomeAmount.value = "";
     return;
   } else {
     console.log(food, rent, clothes);
@@ -37,20 +55,23 @@ function calculateExpenses() {
     balances = income - expenses;
   }
   if (expenses > income) {
-    alert("Expenses cannot be greater than income");
+    calculationAlert.style.display = "block";
+    incomeAmount.value = "";
+    // alert("Expenses cannot be greater than income");
     return;
   } else {
-    console.log(totalExpenses.innerText);
+    alertMessage.style.display = "none";
     totalExpenses.innerText = expenses.toFixed(2);
     totalBalance.innerText = balances.toFixed(2);
     incomeAmount.value = "";
   }
 }
+// calculation of savings and showing an alert if it is not valid
 function calculateSavings() {
   const savingsPercentage = document.getElementById("saving-percentage");
-  const incomeBalanceValue = document.getElementById("income-amount");
   const savings = document.getElementById("saving-balance");
   const remainingBalance = document.getElementById("remaining-balance");
+  const alertSavings = document.getElementById("alertSavings");
   const savingsAmount = parseFloat(savingsPercentage.value);
   if (savingsAmount < 0 || isNaN(savingsAmount)) {
     alert("Please enter a valid number");
@@ -58,7 +79,8 @@ function calculateSavings() {
   } else {
     savingCalculation = income * (savingsAmount / 100);
     if (savingCalculation >= balances) {
-      alert("Savings cannot be greater than balance");
+      // alert("Savings cannot be greater than balance");
+      alertSavings.style.display = "block";
       savingsPercentage.value = "";
       return;
     } else {
@@ -68,6 +90,7 @@ function calculateSavings() {
     }
   }
 }
+// adding event listeners to the calculate and save buttons
 document
   .getElementById("calculate-expenses")
   .addEventListener("click", calculateExpenses);
